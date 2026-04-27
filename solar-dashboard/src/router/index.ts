844c3288d3f6ea76dbc41c89eba75895e7683e6d
mwcart2001-commits <mwcart2001@gmail.com>
2026-04-27 16:41:48 -0400

    Define routes for Dashboard and Current System pages

--- solar-dashboard/src/router/index.ts
+++ solar-dashboard/src/router/index.ts
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DashboardPage from '../pages/DashboardPage.vue'
+import CurrentSystemPage from '../pages/CurrentSystemPage.vue'
 
-const router = createRouter({
+const routes = [
+  { path: '/', component: DashboardPage },
+  { path: '/current-system', component: CurrentSystemPage }
+]
+
+export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
-
-export default router
