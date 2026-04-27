88c85e8bf9466ab3bcd716e7b88822f2458cfcc1
mwcart2001-commits <mwcart2001@gmail.com>
2026-04-27 19:41:08 -0400

    Add PanelTile component with label prop

---
+++ solar-dashboard/src/components/PanelTile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="panel-tile flex items-center justify-center border rounded-md h-20 w-20 bg-gray-100"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String
+})
+</script>
